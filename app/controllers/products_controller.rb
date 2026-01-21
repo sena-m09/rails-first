5b41034924ca3e8d45beb8fcd4c8b34bbc7b9977
sena-m09 <sena-murakami@gaji.jp>
2026-01-21 09:09:06 +0900

    Add create/edit/update actions to ProductsController

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,31 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def product_params
+      params.expect(product: [ :name ])
+    end
 end
